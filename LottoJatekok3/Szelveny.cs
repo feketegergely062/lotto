b13341d0ed9bb982606ae82b81d3cbc4448aa190
gergely.fekete <gergely.fekete@gmail.com>
2023-11-17 15:26:56 +0100

    first commit

---
+++ LottoJatekok3/Szelveny.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LottoJatekok3
+{
+    internal class Szelveny:TableLayoutPanel
+    {
+        private int sor;
+        private int oszlop;
+        private int tippdb;
+
+        public Szelveny(int sor, int osz, int tippdb)
+        {
+            this.sor = sor;
+            this.oszlop = osz;
+            this.tippdb = tippdb;
+            this.BackColor = Color.Red;
+        }
+    }
+}
